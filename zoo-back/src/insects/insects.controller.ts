4d6b03baebb46649e5d9800b6921de235bada15e
antho1218 <74335671+antho1218@users.noreply.github.com>
2022-12-04 18:35:20 -0500

    CRUD
    CRUD get post put delete

--- zoo-back/src/insects/insects.controller.ts
+++ zoo-back/src/insects/insects.controller.ts
@@ -1,10 +1,25 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Put, Param, Delete} from  '@nestjs/common';
 import { CreateInsectsDto } from './Dto/create-controller.dto';
 
 @Controller('insects')
 export class InsectsController {
+    @Get()
+    creat(): string {
+        return `This action retuns all insects`;
+    }
+
     @Post()
     async create(@Body() createinsectsdto : CreateInsectsDto ) {
-        return `This action adds a new insects, ${createinsectsdto.name};
+        return `This action adds a new insects, ${createinsectsdto.name}`;
+    }
+   
+    @Put(':id')
+    update (@Param('id') id:string, @Body() createinsectsdto: CreateInsectsDto) {
+      return `This action updates a #${id} dog`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action remove a #${id} dog`;
     }
 }
